9cf388a6b3cff45466e0447ed7da961539110aff
shalu2001 <danthanarayanas.21@uom.lk>
2024-03-10 00:18:27 +0530

    Add dotenv package and update database configuration

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 import  Sequelize  from 'sequelize';
+const {DATABASE_HOST, DATABASE_USERNAME, DATABSE_PASSWORD, DATABASE_NAME } = process.env;
 
-const sequelize = new Sequelize('cms', 'root', 'pass123', {
-  host: 'localhost',
+const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME,DATABSE_PASSWORD, {
+  host: DATABASE_HOST,
   dialect: 'mysql',
 });
+
+
 
 export default sequelize;
