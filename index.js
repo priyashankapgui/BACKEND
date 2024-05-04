fd986bde3a6de7f8ca1394128239544edc9f1290
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2024-05-04 09:52:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 import sequelize from "./config/database.js";
 import express from "express";
-import products, {setupProductGRNAssociations} from "./src/modules/product/product.js";
-import productSupplier from './src/modules/product_Supplier/product_Supplier.js';
-import Productrouter from "./src/modules/product/routes.js";
-import categoryRouter from "./src/modules/category/routes.js";
-import supplierRouter from "./src/modules/supplier/routes.js";
-import GRNRouter from "./src/modules/GRN/routes.js";
-import productSupplierRouter from './src/modules/product_Supplier/routes.js';
-//import categories from "./src/modules/category/category.js";
-import suppliers, {setupProductSupplierAssociations} from "./src/modules/supplier/supplier.js";
-import grn from "./src/modules/GRN/grn.js";
-//import invoices, {setupInvoiceAssociations,} from "./src/modules/invoice/invoice.js";
-import { getProducts } from "./src/modules/product/controller.js";
-import { getAllProducts } from "./src/modules/product/service.js";
-import categories, {setupCategoryAssociations} from "./src/modules/category/category.js";
-import cors from "cors"; 
-//import productGRNRouter from "./src/modules/product_GRN/routes.js";
-import { setupAssociations } from "./src/modules/associationSetup.js";
-import Branchrouter from "./src/modules/branch/routes.js";
-import branchSupplierRouter from "./src/modules/branch_Supplier/routes.js";
-import branches, {setupBranchSupplierAssociations} from "./src/modules/branch/branch.js";
- 
+import products from './src/modules/product/product.js';
+import Productrouter from './src/modules/product/routes.js';
+import categoryRouter from './src/modules/category/routes.js';
+import supplierRouter from './src/modules/supplier/routes.js';
+// import stockRouter from './src/modules/stock/routes.js';
+import categories from './src/modules/category/category.js';
+import suppliers from './src/modules/supplier/supplier.js';
+import { getProducts } from './src/modules/product/controller.js';
+import { getAllProducts} from './src/modules/product/service.js';
+import Category, { setupCategoryAssociations } from './src/modules/category/category.js';
+import feedbackrouter from './src/modules/feedback/routes.js';
+import feedback from './src/modules/feedback/feedback.js';
+import cors from 'cors';
+import Branchrouter from './src/modules/branch/routes.js';
+import Branch from './src/modules/branch/branch.js'
+
+
+
+
 const app = express();
  
 app.use(cors());
 
-app.use(express.json());  
-  
-app.use("/", Productrouter);
-app.use("/", categoryRouter);
-app.use("/", supplierRouter);
-app.use("/", GRNRouter);
-app.use('/', productSupplierRouter);
-//app.use('/', productGRNRouter);
+app.use(express.json());
+
+app.use('/', Productrouter);
+app.use('/', categoryRouter);
+app.use('/', supplierRouter);
+// app.use('/', stockRouter);
 app.use('/',Branchrouter);
 app.use('/', branchSupplierRouter);
 
-app.use("/api", Productrouter);  
-app.use("/api", categoryRouter);
-app.use("/api", supplierRouter);
-app.use("/api", GRNRouter);
-app.use('/api', productSupplierRouter);
-//app.use('/api', productGRNRouter);
+app.use('/api', Productrouter);
+app.use('/api', categoryRouter);
+app.use('/api', supplierRouter);
+// app.use('/api', stockRouter);
+app.use('/api',Branch);
+app.use('/api',feedbackrouter);
 
 app.use('/Images', express.static('.src/Images'))
 
