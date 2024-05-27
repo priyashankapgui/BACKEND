40f482a30ae2a5438accc562ce59072d5690142e
Pramudi Gamage <gamagepam.21@uom.lk>
2024-05-27 23:51:18 +0530

    commit replace

--- config/database.js
+++ config/database.js
@@ -39,4 +39,22 @@ const sequelize = new Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSW
   logging: console.log 
 });
 
+<<<<<<< Updated upstream
 export default sequelize;
+=======
+
+
+export default sequelize;
+
+// import  Sequelize  from 'sequelize';
+// const {DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME } = process.env;
+
+// const sequelize = new Sequelize('cms', 'root', 'pass123', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+// });
+
+
+
+// export default sequelize;
+>>>>>>> Stashed changes
