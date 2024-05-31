f51dd04e792f1e09cbf25878bea5bd4a040992c5
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2024-05-31 09:13:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import sequelize from "./config/database.js";
 import express from "express";
 import cors from "cors"; 
-import grn from "./src/modules/GRN/grn.js";
 import dotenv from "./config/database.js";
+import Stripe from 'stripe';
+
+// Importing modules and their routes
+import grn from "./src/modules/GRN/grn.js";
 import products from "./src/modules/product/product.js";
 import suppliers from "./src/modules/supplier/supplier.js";
 import categories from "./src/modules/category/category.js";
@@ -17,24 +20,22 @@ import GRNRouter from "./src/modules/GRN/routes.js";
 import productSupplierRouter from './src/modules/product_Supplier/routes.js';
 import productGRNRouter from "./src/modules/product_GRN/routes.js";
 import branchSupplierRouter from "./src/modules/branch_Supplier/routes.js";
-import { setupAssociations } from "./src/modules/associationSetup.js";
 import listedProductsRouter from "./src/modules/listedProducts/routes.js";
 import billRouter from "./src/modules/bill/routes.js";
 import feedback from "./src/modules/feedback/feedback.js";
 import feedbackrouter from "./src/modules/feedback/routes.js";
 import cartProductRoutes from "./src/modules/cart_Product/routes.js"
 import ShoppingCart from "./src/modules/Cart_Customer/shoppingcart.js";
-import Stripe from 'stripe';
+import { setupAssociations } from "./src/modules/associationSetup.js";
 
-
-
- 
+// Initialize express app
 const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-app.use(cors());
 
-app.use(express.json());  
-  
+app.use(cors());
+app.use(express.json());
+
+// Registering routes
 app.use("/", Productrouter);
 app.use("/", categoryRouter);
 app.use("/", supplierRouter);
@@ -44,18 +45,16 @@ app.use('/', productGRNRouter);
 app.use('/', Branchrouter);
 app.use('/', branchSupplierRouter);
 app.use('/', EmployeeRouter);
-app.use('/', CustomerRouter)
+app.use('/', CustomerRouter);
 app.use('/', listedProductsRouter);
 app.use('/', billRouter);
 app.use('/', feedbackrouter);
 app.use('/', cartProductRoutes);
 
-
-
-app.use("/api", Productrouter);  
-app.use("/api", categoryRouter);
-app.use("/api", supplierRouter);
-app.use("/api", GRNRouter);
+app.use('/api', Productrouter);  
+app.use('/api', categoryRouter);
+app.use('/api', supplierRouter);
+app.use('/api', GRNRouter);
 app.use('/api', productSupplierRouter);
 app.use('/api', EmployeeRouter);
 app.use('/api', CustomerRouter);
@@ -65,42 +64,7 @@ app.use('/api', billRouter);
 app.use('/api', feedback);
 app.use('/api', cartProductRoutes);
 
-
-app.use('/Images', express.static('.src/Images'))
-
-setupAssociations();
-
-
-
-
-sequelize.sync({ alter: true }) 
-  .then(() => {
-    console.log("Database synchronized");
-    app.listen(8080, () => {
-      console.log("Connected to Backend!!");
-    });
-  })
-  .catch((err) => {
-    console.error("Error synchronizing database:", err);
-  });
-
-
-process.on("SIGINT", () => {
-  sequelize
-    .close()
-    .then(() => {
-      console.log("Connection closed.");
-      process.exit(0);
-    })
-    .catch((err) => {
-      console.error("Error closing Sequelize connection:", err);
-      process.exit(1);
-    });
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Promise Rejection:', err);
-});
+app.use('/Images', express.static('.src/Images'));
 
 // Stripe Checkout Session Route
 app.post('/create-checkout-session', async (req, res) => {
@@ -113,9 +77,9 @@ app.post('/create-checkout-session', async (req, res) => {
         price_data: {
           currency: 'lkr',
           product_data: {
-            name: item.name,
+            name: item.productName,
           },
-          unit_amount: item.price * 100,
+          unit_amount: item.sellingPrice * 100,  
         },
         quantity: item.quantity,
       })),
@@ -131,5 +95,35 @@ app.post('/create-checkout-session', async (req, res) => {
   }
 });
 
+// Setting up associations and synchronizing the database
+setupAssociations();
 
- export { sequelize, categories, suppliers, grn, products, branches,feedback,ShoppingCart};
+sequelize.sync({ alter: true }) 
+  .then(() => {
+    console.log("Database synchronized");
+    app.listen(8080, () => {
+      console.log("Connected to Backend!!");
+    });
+  })
+  .catch((err) => {
+    console.error("Error synchronizing database:", err);
+  });
+
+// Handle process exit and unhandled promise rejections
+process.on("SIGINT", () => {
+  sequelize.close()
+    .then(() => {
+      console.log("Connection closed.");
+      process.exit(0);
+    })
+    .catch((err) => {
+      console.error("Error closing Sequelize connection:", err);
+      process.exit(1);
+    });
+});
+
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Promise Rejection:', err);
+});
+
+export { sequelize, categories, suppliers, grn, products, branches, feedback, ShoppingCart };
