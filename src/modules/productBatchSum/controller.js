b3f8472b7bce9633955082615ee7cd46119134a7
Pramudi Gamage <gamagepam.21@uom.lk>
2024-06-26 22:10:58 +0530

    searching products by branch

--- src/modules/productBatchSum/controller.js
+++ src/modules/productBatchSum/controller.js
@@ -305,3 +305,24 @@ export const getUpcomingExpProductBatchSumDataByBranchController = async (req, r
     });
   }
 }
+
+
+
+
+export const getProductDetailsByBranchNameController = async (req, res) => {
+  const { branchName } = req.body;
+  console.log("branchName",branchName);
+
+  try {
+    const products = await ProductBatchSumService.getProductDetailsByBranchName(branchName);
+
+    if (!products.length) {
+      return res.status(404).json({ message: 'No products found for the given branch name' });
+    }
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error('Error fetching product details:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
