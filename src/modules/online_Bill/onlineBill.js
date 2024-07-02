bac22b1a51c5051a5f9a055609fa76f467872384
KithminiOKD <112233369+KithminiOKD@users.noreply.github.com>
2024-07-02 22:57:41 +0530

    BillAmount

--- src/modules/online_Bill/onlineBill.js
+++ src/modules/online_Bill/onlineBill.js
@@ -25,6 +25,10 @@ const onlineBill = sequelize.define('onlineBill', {
       key: 'customerId'
     }
   },
+  totalAmont:{
+    type:DataTypes.FLOAT,
+    allowNull:true,
+  },
   createdAt: {
     type: 'TIMESTAMP',
     defaultValue: DataTypes.NOW,
