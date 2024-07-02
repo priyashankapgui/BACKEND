4ba4b1f02c1389d67b70c3db0363708ca30f912a
Pramudi Gamage <gamagepam.21@uom.lk>
2024-07-02 11:56:01 +0530

    changing stock transfer functions

--- src/modules/stockTransfer/routes.js
+++ src/modules/stockTransfer/routes.js
@@ -4,13 +4,8 @@ import * as Controller from "../stockTransfer/controller.js";
 const stockTransferRouter = express.Router();
 
 stockTransferRouter.post("/stockTransferOUT", Controller.createStockTransferAndProduct);
-stockTransferRouter.get('/stock-transfer/supplying-branch/:supplyingBranch', Controller.fetchStockTransfersBySupplyingBranch);
-stockTransferRouter.get('/stock-transfer/request-branch/:requestBranch', Controller.getStockTransfersByBranch);
 stockTransferRouter.put('/stock-transfer/cancel', Controller.cancelStockTransferBySTN_NO);
-stockTransferRouter.get('/stock-transferDetails/:STN_NO', Controller.getStockTransferDetails);
-stockTransferRouter.get('/stock-transferAllDetails/:STN_NO', Controller.getAllStockTransferDetails);
-stockTransferRouter.get('/stock-transfers-by-date-range', Controller.getStockTransfersByDateRangeController);
-stockTransferRouter.get('/stock-transfers-by-productId/:productId',Controller.getStockTransfersByProductIdController);
+stockTransferRouter.get('/stock-transferAllDetails', Controller.getAllStockTransferDetails);
 stockTransferRouter.get('/batchNumbers', Controller.fetchBatchNumbers);
 stockTransferRouter.get('/allTransfers', Controller.getAllStockTransfersController);
 
