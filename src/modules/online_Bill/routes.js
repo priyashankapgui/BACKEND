3187faa4c4137603749493cc2606a7e7866357ef
shalu2001 <danthanarayanas.21@uom.lk>
2024-07-03 20:40:18 +0530

    onlineBill changes

--- src/modules/online_Bill/routes.js
+++ src/modules/online_Bill/routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import * as onlineBillControllers from './controller.js';
 import validator from './validator.js';
+import { authenticateCustomerToken } from '../../middleware/authenticationMiddleware.js';
 
 const router = express.Router();
 
 router.post('/onlineBills', validator.create, onlineBillControllers.createOnlineBillController);
 router.get('/onlineBills', onlineBillControllers.getAllOnlineBillsController);
 router.get('/onlineBills/:onlineBillNo', onlineBillControllers.getOnlineBillByNumberController);
+router.get('/onlineBillsByCustomer/:customerId', authenticateCustomerToken,onlineBillControllers.getOnlineBillsByCustomerId)
 router.put('/onlineBills/:onlineBillNo', validator.update, onlineBillControllers.updateOnlineBillController);
 
 export default router;
